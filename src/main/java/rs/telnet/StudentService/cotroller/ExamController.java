6e33b7a86f07299806b33a9535695771f8cd62e5
Firstname Lastname <your.email@example.org>
2020-12-01 15:28:32 +0100

    Added get-students-who-failed method

--- src/main/java/rs/telnet/StudentService/cotroller/ExamController.java
+++ src/main/java/rs/telnet/StudentService/cotroller/ExamController.java
@@ -3,8 +3,12 @@ package rs.telnet.StudentService.cotroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rs.telnet.StudentService.dto.ExamResponse;
+import rs.telnet.StudentService.dto.GradeResponse;
+import rs.telnet.StudentService.model.Exam;
+import rs.telnet.StudentService.model.Students;
 import rs.telnet.StudentService.repository.ExamRepository;
 
 import java.util.List;
@@ -21,5 +25,9 @@ public class ExamController {
        return examRepository.findAllExams();
     }
 
+    @GetMapping("/get-students-who-failed")
+    public List<GradeResponse> getStudentsWhoFailed() {
+        return examRepository.findStudentsWhoFailed();
+    }
 
 }
