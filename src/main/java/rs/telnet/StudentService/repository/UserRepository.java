ff8cedd1b15c1a220d1a82772e7e57ffbbbe2865
Firstname Lastname <your.email@example.org>
2020-12-14 10:07:25 +0100

    Update

--- src/main/java/rs/telnet/StudentService/repository/UserRepository.java
+++ src/main/java/rs/telnet/StudentService/repository/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    Optional<org.springframework.security.core.userdetails.User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
