7791fb04f49b05df87e4cdb748ffafad925b543b
Firstname Lastname <your.email@example.org>
2020-12-23 14:25:13 +0100

    Paging and sorting impl

--- src/main/java/rs/telnet/StudentService/repository/StudentsRepository.java
+++ src/main/java/rs/telnet/StudentService/repository/StudentsRepository.java
@@ -1,12 +1,18 @@
 package rs.telnet.StudentService.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rs.telnet.StudentService.model.Students;
+
+import java.util.List;
 
 @Repository
 public interface StudentsRepository extends JpaRepository<Students, String> {
 
+    @Query("Select st from Students st where name like %?1%")
+    Page<Students> findByName(String name, Pageable pageable);
+
 }
-
-
